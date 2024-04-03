c1026a7209fd87242b7e4504ecf4bbd38af48917
Justin9091 <jusraaijmakers@gmail.com>
2024-04-03 07:53:45 +0200

    Cleanup

--- src/components/organisms/OrganismHero.vue
+++ src/components/organisms/OrganismHero.vue
@@ -17,7 +17,6 @@ import {Size} from "@/enums/Size.js";
 
     <div class="img-container">
       <AtomImage class="img" :alt="$t('hero.image.alt')" source="/portfolio/src/assets/img/Ik.png" />
-      <AtomImage class="img" :alt="$t('hero.image.alt')" source="@/assets/img/Ik.png" />
     </div>
   </div>
 </template>
